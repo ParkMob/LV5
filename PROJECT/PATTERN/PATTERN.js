6b58a0bdcd8c0e93b019a6cf643a4d83717a6539
ParkMob <yeojinp0415@gmail.com>
2021-10-21 13:31:57 -0400

    ALL

---
+++ PROJECT/PATTERN/PATTERN.js
@@ -0,0 +1,19 @@
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+multiplyNode(document.querySelector('.CIRCLE1' ), 300, true);
+
+
+
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+      
+      
+    }
+}
+multiplyNode(document.querySelector('.CIRCLE2'),300, true);
